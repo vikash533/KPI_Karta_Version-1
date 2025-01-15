990538460682efc6a864cfa76365749c97ebcfb7
vikash533 <singhvikash533@gmail.com>
2025-01-16 04:26:33 +0530

    Maven Kpi Selenium project setup

---
+++ KPI/src/main/java/Karta/KPI/App.java
@@ -0,0 +1,7 @@
+package Karta.KPI;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ KPI/src/test/java/Karta/KPI/AppTest.java
@@ -0,0 +1,13 @@
+package Karta.KPI;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
