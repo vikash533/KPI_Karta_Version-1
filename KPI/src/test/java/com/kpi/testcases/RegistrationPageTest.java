5c37af44c8c1cbbc05993d365d7e99f98e90ecd4
Nitesh Kumar <niteshkumar@MacBookAir.lan>
2025-02-16 01:14:41 +0530

    Registration Page Validation

--- KPI/src/test/java/com/kpi/testcases/RegistrationPageTest.java
+++ KPI/src/test/java/com/kpi/testcases/RegistrationPageTest.java
@@ -1,5 +1,6 @@
 package com.kpi.testcases;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -12,6 +13,7 @@ import com.kpi.page_object.Registrationpage;
 
 public class RegistrationPageTest extends BaseClass {
 	WebDriver driver;
+	
 	@BeforeClass
 	public void launchBrowser() {
 		try {
@@ -21,10 +23,12 @@ public class RegistrationPageTest extends BaseClass {
 			e.printStackTrace();
 		}
 	}
-//	@AfterClass
+	
+	@AfterClass
 //	public void closeBrowser() {
 //		driver.quit();
 //	}
+	
 	@Test(priority=1)
 	public void registrationPageLanding() {
 		Registrationpage registrationpage = new Registrationpage(driver);
@@ -32,9 +36,31 @@ public class RegistrationPageTest extends BaseClass {
 
 		action.clickOnElement(registrationpage.getCreateAccoutButton());
 		
+		
+		//Title
+		
 	}
-	@Test(priority=2)
-	public void AddRegistrationWithoutData() {
+	
+	@Test (priority=2)
+	public void lableRegistrationPagefield() {
+		Registrationpage registrationpage = new Registrationpage(driver);
+		Actions action = new Actions();
+		
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableSignup()),prop.getProperty("lableSignuptext"),"Lable Signup error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableFullName()),prop.getProperty("lableTxtFullName"),"Lable Full Name error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableEmail()), prop.getProperty("lableTxtEmail", "Lable Email error is not matching"));
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLablePassword()), prop.getProperty("lableTxtPassword"), "Lable Password error is not Matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableConfPassword()), prop.getProperty("lableTxtConfPassword"), "Lable Conf Password error is not Matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableMobile()), prop.getProperty("lableTxtMobile"), "Lable Mobile error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableOrg()), prop.getProperty("lableTxtOrg"), "Lable Org error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableJobTitle()), prop.getProperty("lableTxtJobTitle"), "Lable Job Title error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLablecountry()), prop.getProperty("lableTxtCountry"), "Lable Country error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getBtnNext()), prop.getProperty("lablebtnNext"), "Lable Next BTN error is not Matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getLableBacktoLogin()), prop.getProperty("txtlableBacktoLogin"), "Lable Already account error is not matched");
+	}
+	
+	@Test(priority=3)
+	public void addRegistrationWithoutData() {
 		
 		Registrationpage registrationpage = new Registrationpage(driver);
 		Actions action = new Actions();
@@ -44,13 +70,23 @@ public class RegistrationPageTest extends BaseClass {
 		boolean flag1 = action.isElementVisible(registrationpage.getErrMsgNameFull());
 		Assert.assertTrue(flag1);
 		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgNameFull()),prop.getProperty("errorMsgFullName"),"full name error is not matched");
-		
-		
-		
-		
-		
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgEmail()),prop.getProperty("errorMsgEmail"),"Email error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgPassword()),prop.getProperty("errorMsgPassword"),"Password error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgConfPassword()),prop.getProperty("errorMsgConfPassword"),"Conf Password error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgMobile()),prop.getProperty("errorMsgMobile"),"Mobile error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgOrg()),prop.getProperty("errorMsgOrg"),"Org name error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgJobTitle()),prop.getProperty("errorMsgJobTitle"),"Job Title error is not matched");
+		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getErrMsgCountry()),prop.getProperty("errorMsgCountry"),"Country error is not matched");
 		
 	}
 	
-
+	@Test(priority=4)
+	public void addBlankSpaceRegField() throws InterruptedException {
+		
+		Registrationpage registrationpage = new Registrationpage(driver);
+		Actions action = new Actions();
+//		registrationpage.setTxtFullname().sendKeys(Keys.SPACE);
+//		Thread.sleep(3000);
+//		Assert.assertEquals(action.isElementvisibleTrue(registrationpage.getblkErrorName()), prop.getProperty("blankfullNameError"), "Blank full Name error is not matched");
+	}
 }
